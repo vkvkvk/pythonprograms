8064bbff1c7ba16124e79c6d8f068b9e95d31c32
vidya kulkarni <vpssk2011@gmail.com>
2013-10-19 22:33:27 +0800

    init

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 pythonprograms
 ==============
 ##This is my repository of python programs
+## Now I am modifying it on 19-10-2013
---
+++ hello1.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+
+app =Flask(_name_)
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
